9645ba09477bf0d0f81d28dc53ff1ae7b0c5b4cb
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2023-07-20 23:16:43 -0300

    get ip by host

--- app/app.go
+++ app/app.go
@@ -1,10 +1,40 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Command line app"
 	app.Usage = "Search ip and server names on internet"
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search Ip address on internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: getIps,
+		},
+	}
 	return app
+}
+
+func getIps(context *cli.Context) {
+	host := context.String("host")
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
