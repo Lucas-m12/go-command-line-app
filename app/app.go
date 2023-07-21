10fe74b26d65b819e6f5ef563a5946c0e9baf27c
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2023-07-20 23:24:30 -0300

    get server by host

--- app/app.go
+++ app/app.go
@@ -12,17 +12,24 @@ func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Command line app"
 	app.Usage = "Search ip and server names on internet"
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search Ip address on internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search Ip address on internet",
+			Flags:  flags,
 			Action: getIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Get server names on internet",
+			Flags:  flags,
+			Action: getServers,
 		},
 	}
 	return app
@@ -36,5 +43,16 @@ func getIps(context *cli.Context) {
 	}
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func getServers(context *cli.Context) {
+	host := context.String("host")
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
